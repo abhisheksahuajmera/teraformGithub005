fa41873d855293ce58ae2a62de0a6533b0b1a146
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-11-24 11:39:18 +0530

    All Env dv qa-In Single Push - Try 100

---
+++ mainqa.tf
@@ -0,0 +1,39 @@
+// Executer-provider-location-{name+Version}-{Tag-{Name+Version}+{env|version}
+// Az-Ind-{Rg100}-{Tag-{Rg100}+{qa|100}
+provider "azurerm" {
+  features {}
+}
+//
+variable "rgasenv_name" {
+  type        = string
+  description = "(Optional) The prefix which should be used for all resources in this example. Defaults to burrito."
+  default     = "rg"
+}
+
+variable "rgasenv_location" {
+  type        = string
+  description = "(Optional) The Azure Region in which all resources in this example should be created. Defaults to East US."
+  default     = "eastus"
+}
+//
+variable "env_name" {
+  type        = string
+  description = "(Required) The environment in which all resources in this example should be created."
+  default     = "qa"
+}
+
+variable "env_version" {
+  type        = string
+  description = "(Required) Update to force a change."
+  default     = "100"
+}
+//
+resource "azurerm_resource_group" "rg105qatg" {
+  location = var.rgasenv_location
+  name     = "${var.rgasenv_name}-${var.env_name}"
+
+  tags = {
+    environment = var.env_name
+    version     = var.env_version
+  }
+}
